332a2bb6f8375d36867d0927a4c053760fcef34d
jdavis <joedavis29@gmail.com>
2022-07-30 12:02:46 -0400

    limit max records

--- App.tsx
+++ App.tsx
@@ -1,8 +1,15 @@
 import App from './src';
 import {Amplify, AuthModeStrategyType} from 'aws-amplify';
 import awsconfig from './src/aws-exports';
+
 Amplify.configure({
   ...awsconfig,
+  // syncExpressions: [
+  //   syncExpression<Session, []>(Session, () => {
+  //     return (s)=> s.createdAt('gt', (Date.now()-(24*60*60*1000)).toString());
+  //   }),
+  // ],
+  maxRecordsToSync: 20,
   DataStore: {
     authModeStrategyType: AuthModeStrategyType.DEFAULT,
   },
