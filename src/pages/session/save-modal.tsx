00982550663955e3c9d9d6ecb8b21c85bba1b4c8
jdavis <joedavis29@gmail.com>
2022-07-30 12:13:08 -0400

    refactor modal

---
+++ src/pages/session/save-modal.tsx
@@ -0,0 +1,111 @@
+import React, { useContext, useState, useEffect } from 'react';
+import { DataStore } from 'aws-amplify';
+import {View, Dimensions } from 'react-native';
+import { LineChart } from "react-native-chart-kit";
+import * as MetaWear from '../../device/ios/metawear'
+
+import globalStyles, { ThemeType } from '../../styles';
+import {Button, Drawer,withTheme } from 'react-native-paper';
+import DeviceContext from '../../device/ios/device-context';
+import { Tag } from '../../models'
+import { Alert, Modal, Text, StyleSheet} from "react-native";
+
+import { SelectableTag, saveSession } from './save-session';
+import {styles} from './styles'
+
+type TagProps = {
+  tag: SelectableTag
+  setTag: () => void
+  isSelected: Boolean
+}
+
+const Tags = ({tag, setTag, isSelected}: TagProps)=> {
+  return (
+    <Button
+      color='gray'
+      style={{backgroundColor: isSelected ? 'rgba(0,100,0,0.12)': 'rgba(0,0,0,0)', margin:2 }}
+      mode="text"
+      onPress={setTag}
+    >
+      <View style={{ width:'100%', flexDirection: 'row', justifyContent:"space-between"}} >
+        <Text>{tag.name}</Text>
+        <Text>{tag.value} </Text>
+      </View>
+    </Button>
+  )
+}
+
+type SaveModalProps = {
+  modalVisible: boolean
+  setModalVisible: (v:boolean)=>void
+  theme:  ThemeType
+  onSave: (tags: {[key:string]: Boolean} ) => void
+}
+
+export const SaveModal = ({modalVisible, setModalVisible, theme, onSave}: SaveModalProps) => {
+  const {colors} = theme
+  const [tags, settags] = useState<Tag[]>([])
+  const [selectedTags, setselectedTags] = useState<{[key:string]: Boolean}>({})
+  
+  useEffect(() => {
+    const init = async () => {
+      let tags = await DataStore.query(Tag)
+      setModalVisible(true)
+      settags( tags )
+    }
+    init()
+  }, [])
+  
+  const setTag = (Tag: Tag) => {
+    setselectedTags({
+      ...selectedTags,
+      [Tag.id]: !selectedTags[Tag.id]
+    })
+  }
+  
+  return (
+    <Modal
+    animationType="fade"
+    transparent={true}
+    visible={modalVisible}
+    onRequestClose={() => {
+      Alert.alert("Modal has been closed.");
+      setModalVisible(!modalVisible);
+    }}
+  >
+    <View style={styles.centeredView}>
+      <View style={styles.modalView}>
+        <Drawer.Section title="Tags" style={{width:'100%'}}>
+          {tags.map(t => (
+            <Tags
+              tag={t}
+              key={t.id}
+              isSelected={selectedTags[t.id]}
+              setTag={() => setTag(t)}
+            />
+          ))}
+        </Drawer.Section>
+      <View style={{width: '100%', justifyContent:"space-between"}}>
+        <View style={{flexDirection:'row', justifyContent:'space-between'}}>
+          <Button
+            mode="contained"
+            style={{backgroundColor: colors.primary, margin: '5%', width:'40%'}}
+            onPress={() => onSave(selectedTags)}
+          >
+            Save
+          </Button>
+          <Button
+            mode="contained"
+            style={{backgroundColor: colors.error, margin: '5%', width:'40%'}}
+            onPress={async () => {
+              setModalVisible(false)
+            }}>
+            cancel
+          </Button>
+        </View>
+      </View>
+    </View>
+    </View>
+  </Modal>
+  )
+}
--- src/pages/session/session.tsx
+++ src/pages/session/session.tsx
@@ -8,31 +8,11 @@ import globalStyles, { ThemeType } from '../../styles';
 import {Button, Drawer,withTheme } from 'react-native-paper';
 import DeviceContext from '../../device/ios/device-context';
 import { Tag } from '../../models'
-import { Alert, Modal, Text, StyleSheet} from "react-native";
+import { Text} from "react-native";
 
 import { SelectableTag, saveSession } from './save-session';
+import { SaveModal } from './save-modal';
 
-interface TagProps {
-  tag: SelectableTag
-  setTag: () => void
-  isSelected: Boolean
-}
-
-const Tags = ({tag, setTag, isSelected}: TagProps)=> {
-  return (
-    <Button
-      color='gray'
-      style={{backgroundColor: isSelected ? 'rgba(0,100,0,0.12)': 'rgba(0,0,0,0)', margin:2 }}
-      mode="text"
-      onPress={setTag}
-    >
-      <View style={{ width:'100%', flexDirection: 'row', justifyContent:"space-between"}} >
-        <Text>{tag.name}</Text>
-        <Text>{tag.value} </Text>
-      </View>
-    </Button>
-  )
-}
 
 interface SessionScreenProps {
   theme: ThemeType
@@ -44,7 +24,6 @@ const SessionScreen = ({theme}: SessionScreenProps) => {
   const [acc, setacc] = useState<number[][]>([[0],[0],[0]])
   const [gyro, setgyro] = useState<number[][]>([[0],[0],[0]])
   const [tags, settags] = useState<Tag[]>([])
-  const [selectedTags, setselectedTags] = useState<{[key:string]: Boolean}>({})
   const [isStreaming, setisStreaming] = useState(false)
   const [streamingDataExists, setstreamingDataExists] = useState(false)
   const [modalVisible, setModalVisible] = useState(false);
@@ -76,11 +55,10 @@ const SessionScreen = ({theme}: SessionScreenProps) => {
     ]))
   }
 
-  const setTag = (Tag: Tag) => {
-    setselectedTags({
-      ...selectedTags,
-      [Tag.id]: !selectedTags[Tag.id]
-    })
+  
+  const onSave = (selectedTags:{[key:string]: Boolean}) => {
+    saveSession(acc, gyro, tags.filter(t => t.id in selectedTags), 0, 25)
+    setModalVisible(false)
   }
 
   return (
@@ -152,110 +130,21 @@ const SessionScreen = ({theme}: SessionScreenProps) => {
           mode="contained"
           style={{backgroundColor: colors.success, margin: '2%'}}
           disabled={acc[0].length == 0}
-          onPress={async () => {
-            let tags = await DataStore.query(Tag)
-            setModalVisible(true)
-            settags( tags )
-          }}>
+          onPress={async () => setModalVisible(true)}
+        >
             Save
         </Button>
       }
     </Drawer.Section>
-    <Modal
-        animationType="fade"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          Alert.alert("Modal has been closed.");
-          setModalVisible(!modalVisible);
-        }}
-      >
-        <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-            <Drawer.Section title="Tags" style={{width:'100%'}}>
-              {tags.map(t => (
-                <Tags
-                  tag={t}
-                  key={t.id}
-                  isSelected={selectedTags[t.id]}
-                  setTag={() => setTag(t)}
-                />
-              ))}
-            </Drawer.Section>
-          <View style={{width: '100%', justifyContent:"space-between"}}>
-            <View style={{flexDirection:'row', justifyContent:'space-between'}}>
-              <Button
-                mode="contained"
-                style={{backgroundColor: colors.primary, margin: '5%', width:'40%'}}
-                onPress={async () => {
-                  saveSession(acc, gyro, tags.filter(t => t.id in selectedTags), 0, 25)
-                  setModalVisible(false)
-                }}>
-                Save
-              </Button>
-              <Button
-                mode="contained"
-                style={{backgroundColor: colors.error, margin: '5%', width:'40%'}}
-                onPress={async () => {
-                  setModalVisible(false)
-                }}>
-                cancel
-              </Button>
-            </View>
-          </View>
-        </View>
-        </View>
-      </Modal>
+    <SaveModal
+        modalVisible={modalVisible}
+        setModalVisible={setModalVisible}
+        theme={theme}
+        onSave={onSave}
+    />
     </View>
   );
 };
-
-
-const styles = StyleSheet.create({
-  centeredView: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 22
-  },
-  modalView: {
-    width:'80%',
-    margin: 20,
-    backgroundColor: "white",
-    borderRadius: 15,
-    padding: 10,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5
-  },
-  button: {
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2
-  },
-  buttonOpen: {
-    backgroundColor: "#F194FF",
-  },
-  buttonClose: {
-    backgroundColor: "#2196F3",
-  },
-  textStyle: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center"
-  },
-  modalText: {
-    textTransform: 'uppercase',
-    margin: 15,
-    textAlign: "center"
-  }
-});
 
 
 export default withTheme(SessionScreen)
